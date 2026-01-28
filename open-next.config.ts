bc84383cc6737e74f02db36578fa0f805ba9d90e
Everless <admin@southcat.net>
2026-01-28 21:50:07 +0800

    feat: 添加 Cloudflare Pages 部署支持 (OpenNext)

---
+++ open-next.config.ts
@@ -0,0 +1,10 @@
+import type { OpenNextConfig } from '@opennextjs/cloudflare'
+
+export default {
+  default: {
+    override: {
+      wrapper: 'cloudflare-node',
+      converter: 'edge',
+    },
+  },
+} satisfies OpenNextConfig
